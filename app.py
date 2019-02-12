7fa6a7b770c01321ed80b6a7e0469a00ef2295db
electricspacewizard <atz_33@hotmail.com>
2019-02-12 14:52:01 +0000

    Postgress queries now return as JSON objects

--- app.py
+++ app.py
@@ -1,51 +1,52 @@
-from flask import Flask
+from flask import Flask, redirect
 from flask import render_template
-from models import db
+# from models import db
 import psycopg2
 import sys
 import pprint
 from flask import request
+import pdb
 
 
 app = Flask(__name__)
 app.debug = True
 
 POSTGRES = {
-    'user': 'atz',
-    'pw': 'password',
+    'user': 'marina',
+    'pw': '',
     'db': 'marina',
     'host': 'localhost',
-    'port': '5432',
+    'port': '5432'
 }
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
-%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
-db.init_app(app)
+
+#BUGS - wont let you input loa as a int
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
+# %(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+# db.init_app(app)
 
 
 @app.route("/", methods=["POST", "GET"])
 def home():
-    if request.method == "GET":
-        return render_template('home.html')
-    if request.method == "POST":
-        searched_ship = request.form['search']
-        # Validation on search value
-        return boat(searched_ship)
+    if request.args.get('search'):
+        searched_boat = request.args.get('search')
+        return redirect('/boat/' + searched_boat)
+    else:
+      return render_template('home.html')
 
-
-@app.route("/boat/tootsie")
-def boat(searched_ship):
-    boat_data = do_search(searched_ship)
-    print(boat_data[0][0])                                  # CANNOT SEEM TO ACCESS DICT OR LIST HERE
-    return render_template('boat.html', data=boat_data)
+@app.route("/boat/<name>")
+def boat(name):
+    boat_data = do_search(name)
+    return render_template('boat.html', boat=boat_data)
 
 
 def do_search(searched_ship):
-    conn_string = "host='localhost' dbname='marina' user='atz' password='password'"
+    conn_string = "host='localhost' dbname='marina' user='marina' password='password'"
     conn = psycopg2.connect(conn_string)
     cursor = conn.cursor()
     cursor.execute("SELECT row_to_json(boats) FROM boats WHERE bname = '" + searched_ship + "'")
-    return cursor.fetchall()
+    return cursor.fetchall()[0][0]
 
 
 def addboatdatabase(bname, btype, loa):
